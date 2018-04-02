7f72baa0fcbe44d54959deee4b14f8545ac68d17
Adrian Brown <adrian.angel.brown@gmail.com>
2018-04-02 15:43:46 -0400

    feature: created porfolio site & added styles

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import me from './Adrian-bitmoji.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='Adrian__container'>
+        <div className='Adrian__avatar'>
+          <img src={me} alt='Adrian Brown' className='Adrian__img'/>
+        </div>
+        <div className='Adrian__name'>Adrian Brown</div>
+        <div className='Adrian__role'>Software Engineer</div>
+        <div className='Adrian__links-container'>
+          <div className='Adrian__links-container-div'>
+            <a className='Adrian__link' href='https://tfweather.surge.sh'>Weather</a>
+            <a className='Adrian__link' href='https://github.com/AdrianBrown1'>Github</a>
+            <a className='Adrian__link' href='https://medium.com/@adrian.angel.brown'>Blog</a>
+          </div>
+          <div className='Adrian__links-continer-div'>
+            <a className='Adrian__link' href='mailto:adrian.angel.brown@gmail.com'>Email</a>
+            <a className='Adrian__link' href='https://www.linkedin.com/in/adrianangelbrown/'>LinkedIn</a>
+          </div>
+        </div>
       </div>
     );
   }
